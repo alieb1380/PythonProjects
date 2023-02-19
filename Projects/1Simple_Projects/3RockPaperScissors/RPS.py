a79a36ef68da44547ba32b0c23138ca70d8c1e0d
Ali <alieb1380alieb@gmail.com>
2023-02-19 20:50:53 +0330

    Add Hangman, Tictoctoe and edit Rockpaperscissors

---
+++ Projects/1Simple_Projects/3RockPaperScissors/RPS.py
@@ -0,0 +1,19 @@
+import random
+
+def play():
+    user = input(" what is your choice? 'r' for Rock, 'p' for paper, 's' for scissors:\n ")
+    computer = random.choice(["r", 'p', 's'])
+    
+    if user == computer:
+        return 'It\'s a tie'
+    
+    if is_win(user, computer):
+        return 'You Won!'
+    
+    return "You Lost!"
+
+def is_win(player, opponent):
+    if(player == 'r' and opponent == "s") or (player == "s" and opponent == "p") or (player == "p" and opponent == 'r'):
+        return True
+    
+print(play())
